7468dee9207787a3dbc95f5186231f24136b320e
jgomezz <jgomezz@gmail.com>
2023-12-03 09:47:50 -0500

    Update

--- src/main/java/pe/edu/tecsup/springbootapp/repositories/CategoriaRepository.java
+++ src/main/java/pe/edu/tecsup/springbootapp/repositories/CategoriaRepository.java
@@ -1,5 +1,11 @@
 package pe.edu.tecsup.springbootapp.repositories;
 
+import java.util.List;
+
+import pe.edu.tecsup.springbootapp.entities.Categoria;
+
 public interface CategoriaRepository {
 
+	List<Categoria> listar() throws Exception;
+	
 }
--- src/main/java/pe/edu/tecsup/springbootapp/repositories/CategoriaRepositoryImpl.java
+++ src/main/java/pe/edu/tecsup/springbootapp/repositories/CategoriaRepositoryImpl.java
@@ -1,13 +1,65 @@
 package pe.edu.tecsup.springbootapp.repositories;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import pe.edu.tecsup.springbootapp.entities.Categoria;
+
+/**
+ * Clase para procesar un fila de la consulta
+ * 
+ * @author jgomez
+ *
+ */
+class CategoriaRowMapper implements RowMapper<Categoria> {
+
+	@Override
+	public Categoria mapRow(ResultSet rs, int rowNum) throws SQLException {
+		
+		Categoria categoria = new Categoria();
+		categoria.setId(rs.getLong("id"));
+		categoria.setNombre(rs.getString("nombre"));
+		categoria.setOrden(rs.getInt("orden"));
+		
+		return categoria;
+	}
+
+}
+
+/**
+ * Clase repositorio
+ * 
+ * @author jgomez
+ *
+ */
 @Repository
 public class CategoriaRepositoryImpl implements CategoriaRepository {
 
+	private static Logger log = LoggerFactory.getLogger(CategoriaRepositoryImpl.class);
+
 	@Autowired
-	JdbcTemplate jdbcTemplate;  
-	
+	JdbcTemplate jdbcTemplate;
+
+	@Override
+	public List<Categoria> listar() throws Exception {
+
+		log.info("call listar()");
+
+		String sql = "SELECT * FROM categorias";
+
+		List<Categoria> categorias 
+				= jdbcTemplate.query(sql, new CategoriaRowMapper());
+
+		return categorias;
+
+	}
+
 }
