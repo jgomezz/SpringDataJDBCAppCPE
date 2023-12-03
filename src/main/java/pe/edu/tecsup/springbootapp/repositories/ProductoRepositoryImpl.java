747747145d8bca315b4dccb09988964a6feff64f
jgomezz <jgomezz@gmail.com>
2023-12-03 11:11:23 -0500

    Update

--- src/main/java/pe/edu/tecsup/springbootapp/repositories/ProductoRepositoryImpl.java
+++ src/main/java/pe/edu/tecsup/springbootapp/repositories/ProductoRepositoryImpl.java
@@ -11,22 +11,40 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import pe.edu.tecsup.springbootapp.entities.Categoria;
 import pe.edu.tecsup.springbootapp.entities.Producto;
-
 
 class ProductoRowMapper implements RowMapper<Producto> {
 
 	@Override
 	public Producto mapRow(ResultSet rs, int rowNum) throws SQLException {
-		
-		Producto prod = new Producto();
 
-		// TODO
+		Producto producto = new Producto();
+
+		producto.setId(rs.getLong("id"));
+		producto.setCategorias_id(rs.getLong("categorias_id"));
+		Categoria categoria = new Categoria();
+		categoria.setId(rs.getLong("categorias_id"));
+		categoria.setNombre(rs.getString("categorias_nombre"));
+		producto.setCategoria(categoria);
+		producto.setNombre(rs.getString("nombre"));
+		producto.setDescripcion(rs.getString("descripcion"));
+		producto.setPrecio(rs.getDouble("precio"));
+		if (rs.wasNull())
+			producto.setPrecio(null);
+		producto.setStock(rs.getInt("stock"));
+		producto.setImagen_nombre(rs.getString("imagen_nombre"));
+		producto.setImagen_tipo(rs.getString("imagen_tipo"));
+		producto.setImagen_tamanio(rs.getLong("imagen_tamanio"));
+		if (rs.wasNull())
+			producto.setImagen_tamanio(null);
+		producto.setEstado(rs.getInt("estado"));
 		
-		return prod;
+		return producto;
 	}
 
 }
+
 @Repository
 public class ProductoRepositoryImpl implements ProductoRepository {
 
@@ -39,10 +57,15 @@ public class ProductoRepositoryImpl implements ProductoRepository {
 	public List<Producto> listar() throws Exception {
 		log.info("call listar()");
 
-		String sql = "SELECT * FROM productos";
+		String sql =  " SELECT p.id, p.categorias_id, c.nombre AS categorias_nombre, "
+					+ " p.nombre, p.descripcion, p.precio, p.stock, p.imagen_nombre, "
+					+ " p.imagen_tipo, p.imagen_tamanio, p.creado, p.estado " 
+					+ " FROM productos p " 
+					+ " INNER JOIN categorias c ON c.id=p.categorias_id "
+					+ " WHERE estado=1 " 
+					+ " ORDER BY id";
 
-		List<Producto> productos 
-				= jdbcTemplate.query(sql, new ProductoRowMapper());
+		List<Producto> productos = jdbcTemplate.query(sql, new ProductoRowMapper());
 
 		return productos;
 	}
